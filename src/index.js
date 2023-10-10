bd0dcf55daf456be201492c24966468433df0100
Md Mahadi Hasan Rifat <123451855+Rifat8080@users.noreply.github.com>
2023-10-11 00:03:41 +0600

    React components added

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@fontsource/public-sans';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);

4ef1a140b24e18d8b766dcc755bf6d398ef5cb3c
Naol Kidanu <sabelkidanu18@gmail.com>
2023-10-10 16:56:06 +0300

    Implement mission joining

---
+++ src/components/redux/missions/missionSlice.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const DISPLAY_MISSIONS = "space_travelers/missions/DISPLAY_MISSIONS";
+const JOIN_MISSION = "space_travelers/missions/JOIN_MISSION";
+const LEAVE_MISSION = "space_travelers/missions/LEAVE_MISSION";
+const URL = "https://api.spacexdata.com/v3/missions";
+
+export const displayMissions = createAsyncThunk(DISPLAY_MISSIONS, async () => {
+  const result = [];
+  const response = await fetch(URL);
+  const data = await response.json();
+  data.forEach((element) => {
+    result.push({
+      id: element.mission_id,
+      name: element.mission_name,
+      description: element.description,
+      joined: false,
+    });
+  });
+  return result;
+});
+
+export const joinMission = (payload) => ({
+  type: JOIN_MISSION,
+  payload,
+});
